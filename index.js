7af4f8fae433d333a3b781d39dfe2ffb66d2386a
Bhagyashree Pal <bhagyashreepal8485@gmail.com>
2026-04-11 19:21:30 +0530

    Implementing add notes, update notes and delete notes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send('Hello ji Namaste, I build this');
 });
@@ -34,6 +36,60 @@ app.get("/search",(req,res)=>{
         n.subject.toLowerCase().includes(subject.toLowerCase())
     );
     res.json(searchNotes);
+});
+
+app.post("/notes",(req,res)=>{
+    const {subject,level}=req.body;
+    if(!subject||!level){
+        return res.status(404).json({message:"Subject and level are required"});
+    }
+    const newNote={
+        id:notes.length+1,
+        subject,
+        level
+    }
+    notes.push(newNote);
+    res.status(200).json({
+        message:"Node added successfully",
+        note:newNote
+    });
+});
+
+app.put("/notes/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const {subject,level}=req.body;
+    const findNotes=notes.find(n=>n.id===id)
+    if(!findNotes){
+        return res.status(404).json({message:"Notes not found"});
+    }
+    if(subject){
+        findNotes.subject=subject;
+    }
+    if(level){
+        findNotes.level=level;
+    }
+    res.status(200).json({
+        message:"Note updated successfully",
+        note:findNotes
+    });
+});
+
+app.delete("/notes/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const findIndex=notes.findIndex(n=>n.id===id)
+    if(findIndex==-1){
+        return res.status(404).json({message:"Index not found"});
+    }
+
+    console.log("Before delete:", notes);
+
+
+    notes.splice(findIndex, 1);
+    res.status(200).json({
+        message:"Note deleted successfully"
+    });
+
+    console.log("Before delete:", notes);
 });
 
 app.listen(3000, () => {
